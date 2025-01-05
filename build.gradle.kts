2e30bf0d4579197f63ee05ffb4661efe5d27c145
joony.kim <bestheroz@gmail.com>
2025-01-05 19:44:09 +0900

    ⬆️update packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     implementation("com.mysql:mysql-connector-j:9.1.0")
     implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.10.0")
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.4")
-    implementation("io.github.bestheroz:mybatis-repository:0.1.1")
+    implementation("io.github.bestheroz:mybatis-repository:0.3.3")
     implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
     implementation("org.hibernate.orm:hibernate-core:7.0.0.Beta3")
 
