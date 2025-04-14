b514aa9d035458d46307070fcf963f6c59da89e0
joony.kim <bestheroz@gmail.com>
2025-04-14 16:37:37 +0900

    ♻️

--- src/main/kotlin/com/github/bestheroz/standard/config/MyBatisConfig.kt
+++ src/main/kotlin/com/github/bestheroz/standard/config/MyBatisConfig.kt
@@ -2,38 +2,22 @@ package com.github.bestheroz.standard.config
 
 import com.github.bestheroz.standard.common.enums.AuthorityEnum
 import com.github.bestheroz.standard.common.mybatis.handler.GenericListTypeHandler
-import org.mybatis.spring.SqlSessionFactoryBean
 import org.mybatis.spring.annotation.MapperScan
+import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver
-import javax.sql.DataSource
 
 @Configuration
 @MapperScan("com.github.bestheroz")
 class MyBatisConfig {
     @Bean
-    fun sqlSessionFactory(dataSource: DataSource): SqlSessionFactoryBean {
-        val sqlSessionFactoryBean = SqlSessionFactoryBean()
-        sqlSessionFactoryBean.setDataSource(dataSource)
+    fun mybatisConfigurationCustomizer(): ConfigurationCustomizer =
+        ConfigurationCustomizer { configuration ->
+            configuration.isMapUnderscoreToCamelCase = true
 
-        // 매퍼 XML 파일 위치 설정
-        sqlSessionFactoryBean.setMapperLocations(
-            *PathMatchingResourcePatternResolver().getResources("classpath*:mappers/*.xml"),
-        )
-
-        // MyBatis 설정 추가: underscore -> camelCase 매핑 활성화
-        val configuration =
-            org.apache.ibatis.session
-                .Configuration()
-        configuration.isMapUnderscoreToCamelCase = true
-        configuration.typeHandlerRegistry.register(
-            List::class.java,
-            GenericListTypeHandler(AuthorityEnum::class.java),
-        )
-
-        sqlSessionFactoryBean.setConfiguration(configuration)
-
-        return sqlSessionFactoryBean
-    }
+            configuration.typeHandlerRegistry.register(
+                List::class.java,
+                GenericListTypeHandler(AuthorityEnum::class.java),
+            )
+        }
 }
