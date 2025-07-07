9d4b2b025efb6028469437389eb3b70376bb2671
joony.kim <bestheroz@gmail.com>
2025-07-08 08:51:42 +0900

    ⬆️

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("com.google.devtools.ksp") version "2.2.0-2.0.2"
     id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
-    id("com.diffplug.spotless") version "7.0.4"
+    id("com.diffplug.spotless") version "7.1.0"
     id("com.github.ben-manes.versions") version "0.52.0"
     idea
 }
@@ -54,14 +54,14 @@ dependencies {
     implementation("com.mysql:mysql-connector-j:9.3.0")
     implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.11.0")
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.4")
-    implementation("io.github.bestheroz:mybatis-repository:0.8.0")
+    implementation("io.github.bestheroz:mybatis-repository:0.8.1")
     implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
-    implementation("org.hibernate.orm:hibernate-core:7.0.4.Final")
+    implementation("org.hibernate.orm:hibernate-core:7.0.5.Final")
 
     // Logging and Sentry
     implementation("com.auth0:java-jwt:4.5.0")
-    implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.16.0")
-    implementation("io.sentry:sentry-logback:8.16.0")
+    implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.16.1-alpha.2")
+    implementation("io.sentry:sentry-logback:8.16.1-alpha.2")
 
     // OpenAPI
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
@@ -86,8 +86,8 @@ tasks.bootJar {
 
 configure<com.diffplug.gradle.spotless.SpotlessExtension> {
     kotlin {
-        ktfmt("0.53").googleStyle()
-        ktlint("1.5.0").editorConfigOverride(
+        ktfmt("0.56").googleStyle()
+        ktlint("1.6.0").editorConfigOverride(
             mapOf(
                 "ktlint_code_style" to "ktlint_official",
                 "ktlint_standard_no-wildcard-imports" to "disabled",
@@ -97,7 +97,7 @@ configure<com.diffplug.gradle.spotless.SpotlessExtension> {
     }
 
     kotlinGradle {
-        ktlint("1.5.0")
+        ktlint("1.6.0")
     }
 }
 
