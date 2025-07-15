436519f53830f7d791a16e823f9cebcd0b644fa5
joony.kim <bestheroz@gmail.com>
2025-07-15 11:25:11 +0900

    ⬆️

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    val kotlinVersion = "2.2.0"
+    val kotlinVersion = "2.2.20-Beta1"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     kotlin("plugin.jpa") version kotlinVersion
 
-    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
+    id("com.google.devtools.ksp") version "2.2.20-Beta1-2.0.2"
     id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
     id("com.diffplug.spotless") version "7.1.0"
@@ -53,15 +53,15 @@ dependencies {
     // Database
     implementation("com.mysql:mysql-connector-j:9.3.0")
     implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.11.0")
-    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.4")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.5")
     implementation("io.github.bestheroz:mybatis-repository:0.8.1")
     implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
-    implementation("org.hibernate.orm:hibernate-core:7.0.5.Final")
+    implementation("org.hibernate.orm:hibernate-core:7.0.6.Final")
 
     // Logging and Sentry
     implementation("com.auth0:java-jwt:4.5.0")
-    implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.16.1-alpha.2")
-    implementation("io.sentry:sentry-logback:8.16.1-alpha.2")
+    implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.17.0")
+    implementation("io.sentry:sentry-logback:8.17.0")
 
     // OpenAPI
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
@@ -87,7 +87,7 @@ tasks.bootJar {
 configure<com.diffplug.gradle.spotless.SpotlessExtension> {
     kotlin {
         ktfmt("0.56").googleStyle()
-        ktlint("1.6.0").editorConfigOverride(
+        ktlint("1.7.0").editorConfigOverride(
             mapOf(
                 "ktlint_code_style" to "ktlint_official",
                 "ktlint_standard_no-wildcard-imports" to "disabled",
@@ -97,7 +97,7 @@ configure<com.diffplug.gradle.spotless.SpotlessExtension> {
     }
 
     kotlinGradle {
-        ktlint("1.6.0")
+        ktlint("1.7.0")
     }
 }
 
