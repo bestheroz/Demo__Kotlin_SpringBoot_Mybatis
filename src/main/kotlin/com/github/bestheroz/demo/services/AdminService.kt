c2b6d7bac59d4000a1b7bd02df8a7074dcd56dc2
joony.kim <bestheroz@gmail.com>
2025-07-18 11:27:34 +0900

    ♻️

--- src/main/kotlin/com/github/bestheroz/demo/services/AdminService.kt
+++ src/main/kotlin/com/github/bestheroz/demo/services/AdminService.kt
@@ -22,7 +22,6 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional
 class AdminService(
     private val adminRepository: AdminRepository,
     private val jwtTokenProvider: JwtTokenProvider,
